413d108b2f00ff66bfb15164973978fe14e24f9a
Nima Moghadam <nima.73sm@yahoo.com>
2024-05-19 12:13:52 +0330

    update recipe type

--- src/types/Recipe-Interface.tsx
+++ src/types/Recipe-Interface.tsx
@@ -1,4 +1,17 @@
 // ---------- edamam  -------------
+
+interface Image {
+  url: string;
+  width: number;
+  height: number;
+}
+
+interface INutrients {
+  label: string;
+  quantity: number;
+  unit: string;
+}
+
 export interface IRecipe {
   id?: string;
   uri?: string;
@@ -11,24 +24,44 @@ export interface IRecipe {
   mealType: string[];
   totalWeight: number;
   yield: number;
+  totalCO2Emissions: number;
+  healthLabels: string[];
+  shareAs?:string
+  cautions?: string[]
+
   digest: {
     daily: number;
     label: string;
     unit: string;
   }[];
- 
+
+  images: {
+    THUMBNAIL?: Image;
+    SMALL?: Image;
+    REGULAR?: Image;
+    LARGE?: Image;
+  };
+
+  totalNutrients: {
+    ENERC_KCAL?: INutrients;
+    FAT?: INutrients;
+    FASAT?: INutrients;
+    FATRN?: INutrients;
+    FAMS?: INutrients;
+    FAPU?: INutrients;
+    CHOCDF?: INutrients;
+    FIBTG?: INutrients;
+    SUGAR?: INutrients;
+  };
+
+  ingredients: {
+    text: string;
+    quantity: number;
+    measure?: string;
+    food: string;
+    weight: number;
+    foodCategory: string;
+    foodId: string;
+    image?: string;
+  }[];
 }
-
-// ---------- spoonacular  -------------
-
-// export interface IRecipe {
-//   id?: string;
-//   title: string;
-//   image: string;
-//   readyInMinutes: number
-//   summary: string
-//   dishTypes: string[]
-//   vegan: boolean
-//   veryPopular: boolean
-//   pricePerServing: number
-// }
