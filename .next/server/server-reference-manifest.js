a1c09fbcb197ea3164ff902d72c058ab41f6e324
Nima Moghadam <nima.73sm@yahoo.com>
2024-06-05 14:40:21 +0330

    next cache

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"zxCoyet2DPqEe9SeVK54A1UmZjKSOBF5UmnyS1Qf55s=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"efn2hPGhmusFifSbYaqgjvdwnDWGq2S+pW1xQ+kW3J8=\"\n}"
